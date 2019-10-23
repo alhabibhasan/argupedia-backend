50814a9a04d7671ce4eafe35f0b494700ba3a273
Muhammed Hasan <muhammedahasan@outlook.com>
2019-10-23 22:18:31 +0100

    WIP: Move function to get arg chain out of index

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const app = express();
-const port = 3000;
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-const neo4j = require('neo4j-driver').v1;
+const neo4j = require('neo4j-driver').v1
 
-app.use(bodyParser.json());
+const api = require('./api/api')
 
-app.use('/', cors(), (req, res, next) => {
-    res.send({
-        'data': 'Hello world'
-    })
-})
+app.use(bodyParser.json())
 
-const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'password'))
+app.use('/api', cors(), api)
+
+const driver = neo4j.driver(process.env.NEO_HOST, neo4j.auth.basic(process.env.NEO_USERNAME, process.env.NEO_PASS))
 
 const session = driver.session()
 
@@ -31,12 +29,12 @@ const createArg = (statement) => {
         .then(data => {
             let nodeId = neo4j.integer.toNumber(data.records[0]._fields[0])
             return nodeId
-        });
+        })
 }
 
 const respondToArg = (argToRespondToId, responderId, support = true) => {
     // Need to check that a relationship exists between node already before adding a new relationship.
-    if (!isInt(argToRespondToId) || !isInt(responderId)) return 'Need integer ID values';
+    if (!isInt(argToRespondToId) || !isInt(responderId)) return 'Need integer ID values'
 
     const responseType = support ? 'SUPPORTS' : 'ATTACKS'
 
@@ -47,53 +45,25 @@ const respondToArg = (argToRespondToId, responderId, support = true) => {
 
     session.run(cypher, {argToRespondToId: argToRespondToId, responderId: responderId, responseType: responseType})
     .then(data => {
-        console.log(data.records);
-    })
-}
-
-const getRootArgChain = (rootId) => {
-    const noOfNodesInChain = `MATCH p=(rootArg:Argument{root:true})-[*]-(args:Argument) 
-                            WHERE ID(rootArg) = toInteger($rootId)
-                            RETURN COUNT(p) AS NodeCount`
-
-    let noOfNodesPromise = session.run(noOfNodesInChain, {rootId: rootId})
-
-    const cypher = `MATCH p=(rootArg:Argument{root:true})-[*]-(args:Argument) 
-                    WHERE ID(rootArg) = toInteger($rootId)
-                    RETURN rootArg, RELATIONSHIPS(p) AS relationship, args`
-    let rootNodeChainPromise = session.run(cypher, {rootId: rootId})
-  
-    Promise.all([noOfNodesPromise, rootNodeChainPromise])
-    .then(arrayOfResults => {
-        let noOfNodesRes = arrayOfResults[0]
-        let rootNodeChainRes = arrayOfResults[1]
-
-        console.log(neo4j.integer.toNumber(noOfNodesRes.records[0]._fields[0]))
-        console.log(rootNodeChainRes.records)
-
-        /**
-         * TODO: Process the arg chain into a form that can be rendered by the front
-         */
+        console.log(data.records)
     })
 }
 
 const isInt = (value) => {
     return !isNaN(value) && 
             parseInt(Number(value)) == value && 
-            !isNaN(parseInt(value, 10));
+            !isNaN(parseInt(value, 10))
 }
 
 
 // Funct. to create arg
 // createArg('Hello there ' + Math.floor(Math.random() * Math.floor(100))).then(result => {
-//     console.log(result);
-// });
+//     console.log(result)
+// })
 
 
 // Funct. to support args.
 // respondToArg(37, 28, false)
 
-getRootArgChain(23)
 
-
-app.listen(port, () => console.log(`App running on port ${port}!`));
+app.listen(port, () => console.log(`App running on port ${port}!`))
