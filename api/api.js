b4d046238e9ecb5e7af76b9a66e2406094069e33
Muhammed Hasan <muhammedahasan@outlook.com>
2019-10-29 22:27:53 +0000

    Add parameter validation

--- api/api.js
+++ api/api.js
@@ -1,14 +1,47 @@
-const express = require('express');
-const router = express.Router();
-const {getRootArgChain} = require('./read/args');
+const express = require('express')
+const router = express.Router()
+const {getRootArgChain, getRootArgs} = require('./read/getArgs')
+const {createArg} = require('./read/getArgs')
 
-router.get('/getArgChain', (req, res, next) => {
-    getRootArgChain(23)
+const {check, validationResult} = require('express-validator');
+
+const validateId = [
+    check('id').isNumeric({no_symbols: true}).withMessage('arg id must be a whole number')
+]
+
+router.get('/getArgChain/:id', [validateId, validParams], (req, res, next) => {
+    getRootArgChain(req.params.id)
     .then(argChain => {
         res.send({
             argChain
         })
     })
 })
+
+router.get('/getRootArgs', (req, res, next) => {
+    getRootArgs()
+    .then(rootArgs => {
+        res.send({
+            rootArgs
+        })
+    })
+})
+
+/**
+ * This function will check if the validation process has passed or failed.
+ * If failed will set approriate headers to response and will return false. Otherwise, return true.
+ * @param req
+ */
+function validParams(req, res, next) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.status(422).json({
+            errors: errors.array()
+        });
+        return res.send();
+    }
+    next()
+}
 
 module.exports = router
