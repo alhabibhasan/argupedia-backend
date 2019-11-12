730aa8d49f7734f3971774746d53c25107c8ea25
Muhammed Hasan <muhammedahasan@outlook.com>
2019-11-12 00:10:11 +0000

    Change port number to 8080

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
