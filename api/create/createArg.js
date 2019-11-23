7cd56e8738d70b01a5cbe3dbd14f9351a62098c1
Muhammed Hasan <muhammedahasan@outlook.com>
2019-11-23 19:42:45 +0000

    Move code to add attacker to separate file.

--- api/create/createArg.js
+++ api/create/createArg.js
@@ -18,6 +18,29 @@ const createArg = (statement) => {
         })
 }
 
+const respondToArg = (argToRespondToId, responderId, responseType = 'ATTACK') => {
+    // Need to check that a relationship exists between node already before adding a new relationship.
+    if (!isInt(argToRespondToId) || !isInt(responderId)) return 'Need integer ID values'
+
+    const cypher = `MATCH (argToRespondTo:Argument) WHERE ID(argToRespondTo) = toInteger($argToRespondToId)
+                    MATCH (respondingArg:Argument) WHERE ID(respondingArg) = toInteger($responderId)
+                    CREATE (respondingArg)-[r:` + responseType.toUpperCase() + `]->(argToRespondTo) RETURN respondingArg, r, argToRespondTo
+    `
+
+    session.run(cypher, {argToRespondToId: argToRespondToId, responderId: responderId, responseType: responseType})
+    .then(data => {
+        console.log(data.records)
+    })
+}
+
+const isInt = (value) => {
+    return !isNaN(value) && 
+            parseInt(Number(value)) == value && 
+            !isNaN(parseInt(value, 10))
+}
+
+
 export default {
-    createArg
+    createArg,
+    respondToArg
 }
