841ea09cb51734afc932d8039ed56878bf665887
Muhammed Hasan <muhammedahasan@outlook.com>
2019-12-16 21:01:58 +0000

    Remove unnecesary lines

--- index.js
+++ index.js
@@ -8,26 +8,13 @@ const bodyParser = require('body-parser')
 
 const neo4j = require('neo4j-driver').v1
 
-const api = require('./api/api')
+const api = require('./api/routes')
 
 app.use(bodyParser.json())
 
 app.use('/api', cors(), api)
 
 const driver = neo4j.driver(process.env.NEO_HOST, neo4j.auth.basic(process.env.NEO_USERNAME, process.env.NEO_PASS))
-
-const session = driver.session()
-
-
-
-// Funct. to create arg
-// createArg('Supported by the owners of Deimler Motor Group.').then(result => {
-//     respondToArg(2, result, 'proved')
-// })
-
-
-// Funct. to support args.
-// respondToArg(2, 54, 'proved')
 
 
 app.listen(port, () => console.log(`App running on port ${port}!`))
