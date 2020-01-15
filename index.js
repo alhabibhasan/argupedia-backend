a5fc2afffed38f809fd2554af559f8b43d94bbfd
Muhammed Hasan <muhammedahasan@outlook.com>
2020-01-15 20:47:11 +0000

    House keeping.

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ const port = 8080
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
-const neo4j = require('neo4j-driver').v1
-
 const api = require('./api/routes')
 
 app.use(bodyParser.json())
 
 app.use('/api', cors(), api)
 
-const driver = neo4j.driver(process.env.NEO_HOST, neo4j.auth.basic(process.env.NEO_USERNAME, process.env.NEO_PASS))
 
 
 app.listen(port, () => console.log(`App running on port ${port}!`))
