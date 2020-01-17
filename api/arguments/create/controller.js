d0b183c0a34115b104e16427efc1208cc0ea283b
Muhammed Hasan <muhammedahasan@outlook.com>
2020-01-17 22:06:13 +0000

    Format check statements.

--- api/arguments/create/controller.js
+++ api/arguments/create/controller.js
@@ -5,23 +5,45 @@ const {check} = require('express-validator');
 const validParams = require('../../util/validate-argument')
 
 const validateArg = [
-    check('statement').isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('circumstance').isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('action').isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('newCircumstance').isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('goal').isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('value').isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('statement')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('circumstance')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('action')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('newCircumstance')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('goal')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('value')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
     check('argumentBasis').not().isEmpty().withMessage('Argument basis is required.'),
     check('root').isIn(['true', 'false'])
 ]
 
 const validateArgResponse = [
     check('parentId').isInt({min: 0}).withMessage('ID for root arg must be numeric and > 0'),
-    check('statement').isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('circumstance').isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('action').isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('argumentBasis').not().isEmpty().withMessage('Argument basis is required.'),
-    check('propertyToRespondTo').not().isEmpty().withMessage('Need a property to respond to.'),
+    check('statement')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('circumstance')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('action')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('argumentBasis')
+        .isString().withMessage('Needs to be a string')
+        .not().isEmpty().withMessage('Argument basis is required.'),
+    check('propertyToRespondTo')
+        .isString().withMessage('Needs to be a string')
+        .not().isEmpty().withMessage('Need a property to respond to.'),
 ]
 
 router.post('/arg', [validateArg, validParams], (req, res, next) => {
