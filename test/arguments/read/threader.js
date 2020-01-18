c98b9a588d35eb226af2cfb9650d841e5ce3d880
Muhammed Hasan <muhammedahasan@outlook.com>
2020-01-18 16:49:20 +0000

    WIP: Add tests for threader.

---
+++ test/arguments/read/threader.js
@@ -0,0 +1,45 @@
+const mocThread= {
+    thread: {
+      node: {id: 144},
+      attackers: [
+        {
+          node: {
+            id: 145
+          },
+          attackers: [
+            {
+              node: {
+                id: 124
+              },
+              attackers: [
+                {
+                  node: {
+                    id: 146
+                  },
+                  attackers: []
+                },
+                {
+                  node: {
+                    id: 147
+                  },
+                  attackers: []
+                }
+              ]
+            }
+          ]
+        },
+        {
+          node: {
+            id: 125
+          },
+          attackers: []
+        },
+        {
+          node: {
+            id: 126
+          },
+          attackers: []
+        }
+      ]
+    }
+  }
