6f4e96180c4c4d75341c55796dfe0ff559f59ee2
Muhammed Hasan <muhammedahasan@outlook.com>
2020-01-20 16:15:51 +0000

    Add tests for threading

--- test/arguments/read/threader.js
+++ test/arguments/read/threader.js
@@ -1,45 +1,234 @@
-const mocThread= {
-    thread: {
-      node: {id: 144},
-      attackers: [
+const assert = require('assert')
+const {getThread} = require('../../../api/arguments/read/threader')
+
+const mockThread = {
+    "thread": {
+      "node": {
+        "id": "144"
+      },
+      "attackers": [
         {
-          node: {
-            id: 145
+          "node": {
+            "id": "145"
           },
-          attackers: [
+          "attackers": [
             {
-              node: {
-                id: 124
+              "node": {
+                "id": "124"
               },
-              attackers: [
+              "attackers": [
                 {
-                  node: {
-                    id: 146
+                  "node": {
+                    "id": "146"
                   },
-                  attackers: []
+                  "attackers": []
                 },
                 {
-                  node: {
-                    id: 147
+                  "node": {
+                    "id": "147"
                   },
-                  attackers: []
+                  "attackers": []
                 }
               ]
             }
           ]
         },
         {
-          node: {
-            id: 125
+          "node": {
+            "id": "125"
           },
-          attackers: []
+          "attackers": [
+            {
+              "node": {
+                "id": "148"
+              },
+              "attackers": []
+            }
+          ]
         },
         {
-          node: {
-            id: 126
+          "node": {
+            "id": "126"
           },
-          attackers: []
+          "attackers": []
+        },
+        {
+          "node": {
+            "id": "127"
+          },
+          "attackers": []
+        },
+        {
+          "node": {
+            "id": "128"
+          },
+          "attackers": []
+        },
+        {
+          "node": {
+            "id": "129"
+          },
+          "attackers": []
+        },
+        {
+          "node": {
+            "id": "130"
+          },
+          "attackers": [
+            {
+              "node": {
+                "id": "149"
+              },
+              "attackers": [
+                {
+                  "node": {
+                    "id": "131"
+                  },
+                  "attackers": []
+                }
+              ]
+            }
+          ]
         }
       ]
     }
   }
+
+const mockNodesWithAttacker = [
+    {
+      node: { id: "144" },
+      attackers: [
+        {
+          id: "145"
+        },
+        {
+          id: "125"
+        },
+        {
+          id: "126"
+        },
+        {
+          id: "127"
+        },
+        {
+          id: "128"
+        },
+        {
+          id: "129"
+        },
+        {
+          id: "130"
+        }
+      ]
+    },
+    {
+      node: {
+        id: "145",
+      },
+      attackers: [
+        {
+          id: "124"
+        }
+      ]
+    },
+    {
+      node: {
+        id: "124",
+      },
+      attackers: [
+        {
+          id: "146"
+        },
+        {
+          id: "147"
+        }
+      ]
+    },
+    {
+      node: {
+        id: "146",
+      },
+      attackers: []
+    },
+    {
+      node: {
+        id: "147",
+      },
+      attackers: []
+    },
+    {
+      node: {
+        id: "125",
+      },
+      attackers: [
+        {
+          id: "148"
+        }
+      ]
+    },
+    {
+      node: {
+        id: "148",
+      },
+      attackers: []
+    },
+    {
+      node: {
+        id: "126",
+      },
+      attackers: []
+    },
+    {
+      node: {
+        id: "127",
+      },
+      attackers: []
+    },
+    {
+      node: {
+        id: "128",
+      },
+      attackers: []
+    },
+    {
+      node: {
+        id: "129",
+      },
+      attackers: []
+    },
+    {
+      node: {
+        id: "130",
+      },
+      attackers: [
+        {
+          id: "149"
+        }
+      ]
+    },
+    {
+      node: {
+        id: "149",
+      },
+      attackers: [
+        {
+          id: "131"
+        }
+      ]
+    },
+    {
+      node: {
+        id: "131",
+      },
+      attackers: []
+    }
+  ]
+
+describe('Thread generation algorithm', () => {
+    describe('#getThread', () => {
+        it ('should correctly generate a threaded post given a set of nodes with their attackers', () => {
+            let thread = getThread(144, mockNodesWithAttacker)
+            assert.deepEqual(thread, mockThread.thread)
+        })
+    })
+})
