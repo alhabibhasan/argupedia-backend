c5c0bc3f978c6ff2a60d97318c5c2dee84236b76
Muhammed Hasan <muhammedahasan@outlook.com>
2020-01-26 20:26:23 +0000

    Move validation to separate file.

--- api/arguments/create/controller.js
+++ api/arguments/create/controller.js
@@ -1,50 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const {createArg, respondToArg} = require('./createArg')
-const {check} = require('express-validator');
 const validParams = require('../../util/validate-argument')
-
-const validateArg = [
-    check('statement')
-        .isString().withMessage('Needs to be a string')
-        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('circumstance')
-        .isString().withMessage('Needs to be a string')
-        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('action')
-        .isString().withMessage('Needs to be a string')
-        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('newCircumstance')
-        .isString().withMessage('Needs to be a string')
-        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('goal')
-        .isString().withMessage('Needs to be a string')
-        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('value')
-        .isString().withMessage('Needs to be a string')
-        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('argumentBasis').not().isEmpty().withMessage('Argument basis is required.'),
-    check('root').isIn(['true', 'false'])
-]
-
-const validateArgResponse = [
-    check('parentId').isInt({min: 0}).withMessage('ID for root arg must be numeric and > 0'),
-    check('statement')
-        .isString().withMessage('Needs to be a string')
-        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('circumstance')
-        .isString().withMessage('Needs to be a string')
-        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('action')
-        .isString().withMessage('Needs to be a string')
-        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
-    check('argumentBasis')
-        .isString().withMessage('Needs to be a string')
-        .not().isEmpty().withMessage('Argument basis is required.'),
-    check('propertyToRespondTo')
-        .isString().withMessage('Needs to be a string')
-        .not().isEmpty().withMessage('Need a property to respond to.'),
-]
+const {validateArg, validateArgResponse} = require('../validation')
 
 router.post('/arg', [validateArg, validParams], (req, res, next) => {
     let createdNodeToReturn
---
+++ api/arguments/validation.js
@@ -0,0 +1,45 @@
+const {check} = require('express-validator');
+
+const validateArg = [
+    check('statement')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('circumstance')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('action')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('newCircumstance')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('goal')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('value')
+        .isString().withMessage('Needs to be a string')
+        .isLength({min: 5}).withMessage('Title must be greater than 5 characters'),
+    check('argumentBasis').not().isEmpty().withMessage('Argument basis is required.'),
+    check('root').isIn(['true', 'false'])
+]
+
+const validateArgResponse = validateArg.concat([
+    check('parentId')
+        .not().isEmpty().withMessage('Parent ID is a required field')
+        .isInt({min: 0}).withMessage('ID for root arg must be numeric and > 0'),
+    check('propertyToRespondTo')
+        .isString().withMessage('Needs to be a string')
+        .not().isEmpty().withMessage('Need a property to respond to.'),
+])
+
+const validateId = [
+    check('id')
+        .not().isEmpty().withMessage('ID is required')
+        .isInt({min: 0}).withMessage('ID for root arg must be numeric and > 0'),
+]
+
+module.exports = {
+    validateArg,
+    validateArgResponse,
+    validateId
+}
