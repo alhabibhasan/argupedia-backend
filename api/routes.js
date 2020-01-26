1da4d42a31dc3c115e435e8ca7f4771a7de396de
Muhammed Hasan <muhammedahasan@outlook.com>
2020-01-26 20:27:13 +0000

    Add endpoints to delete arg.

--- api/routes.js
+++ api/routes.js
@@ -3,11 +3,15 @@ const router = express.Router()
 
 const createArgController = require('./arguments/create/controller')
 const readArgController = require('./arguments/read/controller')
+const updateArgController = require('./arguments/update/controller')
+const deleteArgController = require('./arguments/delete/controller')
 
 const userController = require('./users/controller')
 
 router.use('/arg/create', createArgController)
 router.use('/arg/read', readArgController)
+router.use('/arg/update', updateArgController)
+router.use('/arg/delete', deleteArgController)
 
 router.use('/user', userController)
 
