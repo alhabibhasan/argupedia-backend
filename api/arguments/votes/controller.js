33c9a4778f4ef539d0b973e9a736718360af74b0
Muhammed Hasan <muhammedahasan@outlook.com>
2020-02-10 21:26:55 +0000

    Add auth required middleware to vote routes.

--- api/arguments/votes/controller.js
+++ api/arguments/votes/controller.js
@@ -5,22 +5,37 @@ const {validateId} = require('../validation')
 const {validateUid, validateOptionalUid} = require('../../users/validation')
 const {upvote, downvote, getVotes} = require('./votes')
 const {argumentExistsMiddleware} = require('../argExistsMiddleware')
+const {jwtAuthMiddleware} = require('../../auth/jwt-verify')
 
-router.post('/up/:id', [validateId, validateUid ,validParams, argumentExistsMiddleware], (req, res, next) => {
-    upvote(req.params.id, req.body.uid)
-    .then(response => {
-        res.send(response)
-    })
+router.post('/up/:id', [validateId,
+                        validateUid,
+                        validParams, 
+                        argumentExistsMiddleware, 
+                        jwtAuthMiddleware], 
+(req, res, next) => {
+        upvote(req.params.id, req.body.uid)
+        .then(response => {
+            res.send(response)
+        })
 })
 
-router.post('/down/:id', [validateId, validateUid ,validParams, argumentExistsMiddleware], (req, res, next) => {
+router.post('/down/:id', [validateId, 
+                          validateUid,
+                          validParams, 
+                          argumentExistsMiddleware, 
+                          jwtAuthMiddleware], 
+(req, res, next) => {
     downvote(req.params.id, req.body.uid)
     .then(response => {
         res.send(response)
     })
 })
 
-router.post('/:id', [validateId, validateOptionalUid ,validParams, argumentExistsMiddleware], (req, res, next) => {
+router.post('/:id', [validateId, 
+                     validateOptionalUid,
+                     validParams, 
+                     argumentExistsMiddleware], 
+(req, res, next) => {
     getVotes(req.params.id, req.body.uid)
     .then(response => {
         res.send(response)
