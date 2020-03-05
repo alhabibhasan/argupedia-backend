27c87437402c1a785a1e83216019f1c0b4cc628e
Muhammed Hasan <muhammedahasan@outlook.com>
2020-03-05 16:49:55 +0000

    Fix tests by stubbing block check middleware

--- test/arguments/read/controller.js
+++ test/arguments/read/controller.js
@@ -10,6 +10,7 @@ const sinon = require('sinon')
 const driver = neo4j.driver(process.env.NEO_HOST, 
                             neo4j.auth.basic(process.env.NEO_USERNAME, 
                             process.env.NEO_PASS))
+const getUser = require('../../../api/users/users')
 
 chai.use(chaiHttp)
 
@@ -42,6 +43,7 @@ const  getRandomInt = (min, max) =>  {
 }
 
 let jwtStub
+let getUserStub
 const fakeB64 = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjYzZTllYThmNzNkZWExMTRkZWI5YTY0OTcxZDJhMjkzN2QwYzY3YWEiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiTXVoYW1tZWQgVGhlIEhhc2FuIiwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL2Z5cC1hcmd1cGVkaWEiLCJhdWQiOiJmeXAtYXJndXBlZGlhIiwiYXV0aF90aW1lIjoxNTgxNDUxMDE4LCJ1c2VyX2lkIjoicE8yclo3bjBjQU4yNnVKT0xaUGFtMEdyR0NrMiIsInN1YiI6InBPMnJaN24wY0FOMjZ1Sk9MWlBhbTBHckdDazIiLCJpYXQiOjE1ODE0NTEwMTgsImV4cCI6MTU4MTQ1NDYxOCwiZW1haWwiOiJtdWhhbW1lZC5oYXNhbkBrY2wuYWMudWsiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJtdWhhbW1lZC5oYXNhbkBrY2wuYWMudWsiXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.T1MEKwxPwj-Lxy8XVnjT40XSNFMz3a6QaH1Oz1w0s7sBceneih4jd20oS42T-81I6usGHANkizMoCVGNQ_nHLdFRfcQ-x63aVJrDrGYtE-34J0496n-i3fzczl2Sti0qajHUYf2nRmWM9qH2VrJ_mqerHyFeP8JWweYj2QWFoj75TJDCvMykvRhXQAp1gLwxWusiUgn02iL1uMMQTeE882lHtFur88583cvQ7ngcGOOvtCM4CRn2z9uvdTaWfEE4CY8Ys2RXwYAlQy2-L2JhWI4uvzLO0NrelkPaa3mcy2kwWknBxlx5823_uXXTHqgwuA1wmBF_AhKffASSaEPXwg'
 const fakeSuccessTokenObject = {
     success: 'Token is valid',
@@ -61,6 +63,9 @@ describe('Arguments', () => {
     before((done) => {
         jwtStub = sinon.stub(jwt, 'verify').callsFake(() => {
             return Promise.resolve(fakeSuccessTokenObject);
+        })
+        getUserStub = sinon.stub(getUser, 'getUser').callsFake(() => {
+            return Promise.resolve({blocked: false})
         })
         let createPost = (final) => {
             testArg.statement = JSON.stringify(Math.random())
