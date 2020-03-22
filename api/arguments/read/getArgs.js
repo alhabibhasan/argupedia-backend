77a76e9d22ee3575eac073c5bd5237c3f6a436e1
Muhammed Hasan <muhammedahasan@outlook.com>
2020-03-22 17:23:30 +0000

    Get deleted args.

--- api/arguments/read/getArgs.js
+++ api/arguments/read/getArgs.js
@@ -22,7 +22,6 @@ const getRootArgChain = (rootId) => {
 
     const relationshipsCypher = `MATCH p=(rootArg:Argument)<-[*]-(otherNode) 
                     WHERE ID(rootArg) = toInteger($rootId)
-                    AND otherNode.deleted <> true
                     RETURN rootArg{.*, id: ID(rootArg), type:labels(rootArg)[0]}, 
                            RELATIONSHIPS(p) AS relationship, 
                            otherNode {.*, id: ID(otherNode), type:labels(otherNode)[0]}`
