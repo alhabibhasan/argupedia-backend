856ff8f9f07e650ebfcf9ffdf1db149787298b4c
Muhammed Hasan <muhammedahasan@outlook.com>
2020-03-25 12:52:33 +0000

    Add camelcase converter

---
+++ api/util/formatting.js
@@ -0,0 +1,12 @@
+const camelCaseToSentenceCase = string => {
+    let finalResult = ''
+    if (string && string.length) {
+        let result = string.replace( /([A-Z])/g, " $1" );
+        finalResult = result.charAt(0).toUpperCase() + result.slice(1);
+    }
+    return finalResult
+}
+
+module.exports = {
+    camelCaseToSentenceCase
+}
